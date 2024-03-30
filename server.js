7684c2e2bda9154894df570b9b7f00d8bf0b31f7
adithyan-hermit <93730698+adithyan-hermit@users.noreply.github.com>
2024-03-30 22:39:05 +0530

    Update server.js

--- server.js
+++ server.js
@@ -1,5 +1,5 @@
 const express = require("express");
-const app = express();
+/* const app = express();
 var server = require('http').Server(app);
 
 app.use(express.static("public"));
@@ -10,4 +10,29 @@ app.get("/", (request, response) => {
 
 const listener = server.listen(process.env.PORT || '3000', () => {
   console.log("Your app is listening on port " + listener.address().port);
+}); */
+const http = require('http');
+const httpProxy = require('http-proxy');
+
+// Create a new proxy server instance
+const proxy = httpProxy.createProxyServer({});
+
+// Create a basic HTTP server
+const server = http.createServer((req, res) => {
+  // Proxy the incoming request to the target server
+  proxy.web(req, res, { target: 'http://localhost:3000' });
+});
+
+// Handle proxy errors
+proxy.on('error', (err, req, res) => {
+  console.error('Proxy Error:', err);
+  res.writeHead(500, {
+    'Content-Type': 'text/plain'
+  });
+  res.end('Something went wrong with the proxy :(');
+});
+
+// Listen on port 8000
+server.listen(8000, () => {
+  console.log('Proxy server listening on port 8000');
 });
